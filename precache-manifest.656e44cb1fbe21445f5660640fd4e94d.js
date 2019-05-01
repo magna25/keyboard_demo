a649199e33dd3e160c7e57b474e3ed12808ad244
magna25 <hennymaster1@gmail.com>
2019-04-30 23:51:59 -0600

    Updates

---
+++ precache-manifest.656e44cb1fbe21445f5660640fd4e94d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "849272bd0bb805685336f791f12206af",
+    "url": "/keyboard_demo/index.html"
+  },
+  {
+    "revision": "ae65c07eecf37677153a",
+    "url": "/keyboard_demo/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "1dc8fc4af4f3c9f7cec6",
+    "url": "/keyboard_demo/static/css/main.3219c9ba.chunk.css"
+  },
+  {
+    "revision": "ae65c07eecf37677153a",
+    "url": "/keyboard_demo/static/js/2.1d644085.chunk.js"
+  },
+  {
+    "revision": "1dc8fc4af4f3c9f7cec6",
+    "url": "/keyboard_demo/static/js/main.2c2f1d7a.chunk.js"
+  },
+  {
+    "revision": "e1a2530fa26e9db5c3bc",
+    "url": "/keyboard_demo/static/js/runtime~main.5ef5cec7.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.0/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.59708ca9a629be8a63183659fe154f71.js"
+  "/keyboard_demo/precache-manifest.656e44cb1fbe21445f5660640fd4e94d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/keyboard_demo/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
